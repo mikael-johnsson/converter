1949cdbf1348c46a8ed097e7e5bc92c5771c0510
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-12-19 13:00:45 +0100

    create convertCurrency and calculateUSD func

--- dist/currency.js
+++ dist/currency.js
@@ -1,2 +1,46 @@
 "use strict";
+const currencyInputBox = document.getElementById('currency-input-box');
+const currencyOutputBox = document.getElementById('currency-output-box');
+const currencyConvertButton = document.getElementById('currency-convert-button');
+function convertCurrency() {
+    const fromType = document.getElementById('currency-from-select');
+    const toType = document.getElementById('currency-to-select');
+    const fromTypeValue = fromType.value;
+    const toTypeValue = toType.value;
+    if (fromTypeValue == toTypeValue) {
+        alert('Please select two different types');
+    }
+    else {
+        switch (fromTypeValue) {
+            case "USD" /* Currency.USD */:
+                calculateUSD(parseFloat(currencyInputBox.value), toTypeValue);
+                break;
+            case "EUR" /* Currency.EUR */:
+                break;
+            case "GBP" /* Currency.GBP */:
+                break;
+            case "SEK" /* Currency.SEK */:
+                break;
+            default: console.log('Please select a valid type');
+        }
+    }
+}
+function calculateUSD(input, toType) {
+    switch (toType) {
+        case "EUR" /* Currency.EUR */:
+            currencyOutputBox.innerHTML = `${(input * 0.85).toFixed(2)} EUR`;
+            break;
+        case "GBP" /* Currency.GBP */:
+            currencyOutputBox.innerHTML = `${(input * 0.73).toFixed(2)} GBP`;
+            break;
+        case "SEK" /* Currency.SEK */:
+            currencyOutputBox.innerHTML = `${(input * 8.61).toFixed(2)} SEK`;
+            break;
+        default: console.log('Please select a valid type');
+    }
+}
+currencyConvertButton === null || currencyConvertButton === void 0 ? void 0 : currencyConvertButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    convertCurrency();
+});
 //# sourceMappingURL=currency.js.map
--- src/currency.ts
+++ src/currency.ts
@@ -5,3 +5,53 @@ const enum Currency {
     SEK = 'SEK',
 }
 
+const currencyInputBox = document.getElementById('currency-input-box') as HTMLInputElement;
+const currencyOutputBox = document.getElementById('currency-output-box') as HTMLParagraphElement;
+const currencyConvertButton = document.getElementById('currency-convert-button') as HTMLButtonElement;
+
+function convertCurrency(): void {
+    const fromType = document.getElementById('currency-from-select') as HTMLSelectElement;
+    const toType = document.getElementById('currency-to-select') as HTMLSelectElement;
+    const fromTypeValue = fromType.value as Currency;
+    const toTypeValue = toType.value as Currency;
+    
+    if(fromTypeValue == toTypeValue) {
+        alert('Please select two different types');
+    } else {
+        switch(fromTypeValue) {
+            case Currency.USD:
+                calculateUSD(parseFloat(currencyInputBox.value), toTypeValue);
+                break;
+            case Currency.EUR:
+                break;
+            case Currency.GBP:
+                break;
+            case Currency.SEK:
+                break;
+            default: console.log('Please select a valid type');
+        }
+    }
+}
+
+function calculateUSD(input: number, toType: Currency): void {
+    switch(toType) {
+        case Currency.EUR:
+            currencyOutputBox.innerHTML = `${(input * 0.85).toFixed(2)} EUR`
+            break;
+        case Currency.GBP:
+            currencyOutputBox.innerHTML = `${(input * 0.73).toFixed(2)} GBP`
+            break;
+        case Currency.SEK:
+            currencyOutputBox.innerHTML = `${(input * 8.61).toFixed(2)} SEK`
+            break;
+        default: console.log('Please select a valid type');
+    }
+
+}
+
+currencyConvertButton?.addEventListener('click', (e) => {
+    e.preventDefault();
+    convertCurrency();
+})
+
+
